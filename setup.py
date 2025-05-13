c51dad35adcaf08b52d6f6fc83e32471aebb686e
shivaraj <telishivaraj421@gmail.com>
2025-05-13 11:05:39 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'shivaraj teli',
+    author_email= 'telishivaraj421@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
